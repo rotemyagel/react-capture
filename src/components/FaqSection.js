067c4693f1f9b71abfa33f8ce2d0551d10c93b46
rotemyagel <rotemyagel@gmail.com>
2020-11-26 16:09:21 +0200

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do i start</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore consectetur, vel, blanditiis alias facilis veniam illum deserunt modi minima optio doloribus. Temporibus incidunt ratione provident neque totam dicta dolores fuga.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore consectetur, vel, blanditiis alias facilis veniam illum deserunt modi minima optio doloribus. Temporibus incidunt ratione provident neque totam dicta dolores fuga.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different payment</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore consectetur, vel, blanditiis alias facilis veniam illum deserunt modi minima optio doloribus. Temporibus incidunt ratione provident neque totam dicta dolores fuga.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>what products do you offer</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore consectetur, vel, blanditiis alias facilis veniam illum deserunt modi minima optio doloribus. Temporibus incidunt ratione provident neque totam dicta dolores fuga.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
